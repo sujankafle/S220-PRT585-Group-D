2e38ddfa9b4f1c6cefac77f15c6b87d980b88531
sujankafle <sujan_k@live.com>
2020-09-09 12:28:44 +0930

    Initial Commit

---
+++ CDUNSA.API/Data/DataContext.cs
@@ -0,0 +1,12 @@
+using CDUNSA.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CDUNSA.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base (options){}
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
