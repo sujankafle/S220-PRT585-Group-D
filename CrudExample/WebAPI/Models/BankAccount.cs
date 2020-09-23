105eeab70e7fab36bb66599662422a1aca3d68cc
sujankafle <sujan_k@live.com>
2020-09-23 10:10:56 +0930

    Initial Commit (Models, Connection String)

---
+++ CrudExample/WebAPI/Models/BankAccount.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class BankAccount
+    {
+        [Key]
+        public int BankAccountID { get; set; }
+        [Column(TypeName ="navchar(20)")]
+        [Required]
+        public int AccountNumber { get; set; }
+        [Column(TypeName = "navchar(100)")]
+        [Required]
+        public int AccountHolder { get; set; }
+        [Required]
+        public int BankID { get; set; }
+        [Column(TypeName = "navchar(20)")]
+        [Required]
+        public string IFSC { get; set; }
+    }
+}
